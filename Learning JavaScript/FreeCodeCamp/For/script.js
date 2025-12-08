fefbd7a6a3f0123c2f50a25a82bb2e693e241a67
J4sTDuude <yur4uk2012@gmail.com>
2025-12-08 17:57:43 +0200

    for of usage

--- Learning JavaScript/FreeCodeCamp/For/script.js
+++ Learning JavaScript/FreeCodeCamp/For/script.js
@@ -1,3 +1,25 @@
 for (let i = 0; i < 10; i++) {
   console.log("Hello!");
 }
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+for (const num of numbers) {
+  console.log(num);
+}
+
+const str = "freeCodeCamp";
+
+for (let char of str) {
+  console.log(char);
+}
+
+const people = [
+  { name: "John", age: 30 },
+  { name: "Jane", age: 25 },
+  { name: "Jim", age: 40 },
+];
+
+for (const person of people) {
+  console.log(`${person.name} is ${person.age} years old`);
+}
